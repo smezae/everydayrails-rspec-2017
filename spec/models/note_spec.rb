7b3d07ce33381a51d321877880f45eaeebb39824
Sebastian Meza <sbstnmz@gmail.com>
2018-05-16 16:35:43 -0400

    user and note model specs

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+
+  before do
+    # setup test data
+  end
+
+  describe "search message for a term" do
+    before do
+
+    end
+
+    context "when a match is found" do
+      # matching examples
+    end
+
+
+    context "when no match is found" do
+      # non-matching examples
+    end
+  end
+end
